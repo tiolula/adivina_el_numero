4114b63f96ca1b024f9e8726d1952c56399c8c19
lula <lula@42sp.org.br>
2022-04-26 13:04:35 -0300

    primera version del backend

---
+++ negocio/adivina_el_numero.py
@@ -0,0 +1,5 @@
+def juego(a, b):
+   if a == b:
+      return "Ok"
+   else: 
+      return 'incorrecto'
---
+++ testes/testes_adivina_el_numero.py
@@ -0,0 +1,33 @@
+from unittest import TestCase
+import negocio.adivina_el_numero as negocio
+
+# Ale
+# estan vendo eso?
+# Esto acáááá Quando tipeo acá vees ahsiiiiiiiiiiiiiiiiiiiiii ;) í@
+
+class testeDeExemplo(TestCase):
+
+    def test_jugando_valor_elegido_4_valor_correcto_4_resultado_esperado_Ok(self):
+        # Arrange
+        valor_elegido = 4
+        valor_correcto = 4
+        resultado_esperado = "Ok"
+
+        # Act        
+        resultado = negocio.juego(valor_correcto,valor_elegido)
+
+        # Assert
+        self.assertEqual(resultado_esperado, resultado)
+
+    def test_jugando_valor_elegido_5_valor_esperado_4_resultado_esperado_incorrecto(self):
+        # Arrange
+        valor_elegido = 5   
+        valor_correcto = 4
+        resultado_esperado = "incorrecto"
+
+        # Act        
+        resultado = negocio.juego(valor_correcto,valor_elegido)
+
+        # Assert
+        self.assertEqual(resultado_esperado, resultado)
+        
